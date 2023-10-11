18e60b0d5fdfb53b718677e675ef1ffbe28d34f0
nick23blue <105617864+nick23blue@users.noreply.github.com>
2023-10-11 15:37:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function accomplishments(){
+    document.write("<ol>");
+        document.write("<li>18x All Star</li>");
+        document.write("<li>2008 NBA MVP</li>");
+        document.write("<li>5x NBA Champion</li>");
+        document.write("<li>2x NBA Finals MVP</li>");
+        document.write("<li>2x NBA Scoring Champion</li>");
+        document.write("<li>15x All-NBA Selection</li>");
+        document.write("<li>1997 Slam Dunk Champion");
+        document.write("<li>2x Olympic Gold Medal</li>");
+        document.write("<li>4x NBA All Star Game MVP </li>");
+    document.write("</ol>");
+}
